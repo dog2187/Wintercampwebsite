231ffd0bfc0b194c6269f36a79ae297f95495015
dog2187 <46382997+dog2187@users.noreply.github.com>
2019-01-04 10:28:39 -0800

    Add files via upload

---
+++ Web/typed/type.js
@@ -0,0 +1,75 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    strings: [
+      'I',
+      'Is',
+      'Teh',
+      'Thicc',
+      'DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    ],
+    typeSpeed: 5,
+    backSpeed: 10,
+    backDelay: 1500,
+    loop: true,
+  });
+  var title1 = new Typed('#cardTitle1', {
+    strings: [
+      'It is teh Oofnite',
+    ],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title2 = new Typed('#cardTitle2', {
+    strings: [
+      'Teh button below gives you $1000000',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title3 = new Typed('#cardTitle3', {
+    strings: [
+      'FRUIT NINJA!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title4 = new Typed('#cardTitle4', {
+    strings: [
+      'Youtube',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title5 = new Typed('#cardTitle5', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title6 = new Typed('#cardTitle6', {
+    strings: [
+      'Another Cool Picture!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+});
